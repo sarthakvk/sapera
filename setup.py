cd6dc0f77ce91acb94219aa28b26c255c976f3f1
sarthakchaudhary13 <sarthakchaudhary13@gmail.com>
2020-07-03 17:19:34 +0530

    new stable version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="sapera",
-    version="2.0",
+    version="2.1",
     author="Sarthak Vineet Kumar",
     author_email="sarthakchaudhary13@gmail.com",
     description="A python package for learning algorithms",
