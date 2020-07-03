81967db1b0c66c3c39ac43dea4869e4a69016591
sarthakchaudhary13 <sarthakchaudhary13@gmail.com>
2020-07-03 17:17:01 +0530

    feature: implemented same algo for the day

--- sapera/main/main.py
+++ sapera/main/main.py
@@ -2,6 +2,7 @@
 import json
 import os
 import random
+from datetime import datetime
 
 from sapera.settings import BASE_DIR
 
@@ -16,7 +17,7 @@ def update_data_info():
     print(BASE_DIR)
     if os.name == 'nt':
         os.system("{x}\\sapera\\scraper\\updater.cmd {x}".format(
-            x = BASE_DIR)) #calling updater.cmd 
+            x = BASE_DIR)) #calling updater.cmd
     else:
         os.system("bash {x}/sapera/scraper/updater.sh {x}".format(
             x=BASE_DIR))  # calling the updater.sh
@@ -37,6 +38,7 @@ def download_data():
 
 def random_generator():
     info_file_name = BASE_DIR + "/data/.list-of-algorithms.json"
+    today_algo_file = BASE_DIR + "/data/today.json"
 
     with open(info_file_name, "r") as f:
         data = json.load(f)
@@ -46,4 +48,25 @@ def random_generator():
     topic = data[random.choice(topics)]
     algo = random.choice(topic)
 
-    return algo
+    with open(today_algo_file,"r+") as t:
+        try:
+            today_algo = json.load(t)
+
+            if datetime.today().strftime('%Y-%m-%d') == today_algo['date']:
+                return today_algo['algo']
+            else:
+                today_algo = {
+                    'date':datetime.today().strftime('%Y-%m-%d'),
+                    'algo':algo
+                }
+                t.seek(0)
+                t.write(json.dumps(today_algo,indent=4))
+                return algo
+        except:
+            today_algo = {
+                'date':datetime.today().strftime('%Y-%m-%d'),
+                'algo':algo
+            }
+            t.seek(0)
+            t.write(json.dumps(today_algo,indent=4))
+            return algo
